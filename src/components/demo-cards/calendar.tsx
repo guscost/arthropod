37542191f46095ab4a527ec4a3fb7d1ecd0549ed
Gus Cost <guscost@gmail.com>
2025-06-20 14:06:51 +0000

    Calendar demo

--- src/components/demo-cards/calendar.tsx
+++ src/components/demo-cards/calendar.tsx
@@ -14,6 +14,7 @@ export function CardsCalendar() {
         <Calendar
           numberOfMonths={1}
           mode="range"
+          captionLayout="dropdown"
           defaultMonth={start}
           selected={{
             from: start,
