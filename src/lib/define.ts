2e1ad773b8d1162fcccd5ba4e91695b1dd58f6b5
Gus Cost <guscost@gmail.com>
2026-05-25 21:21:09 -0400

    Updated shadcn

--- src/lib/define.ts
+++ src/lib/define.ts
@@ -107,7 +107,10 @@ const define = (...args: any) => {
     factory = args[2];
 
     // Webpack modules do not load dynamically, add these script tags in order
-    __MODULES__.set(name, factory.call(undefined, ...deps.map(__MODULES__.get)));
+    __MODULES__.set(
+      name,
+      factory.call(undefined, ...deps.map(__MODULES__.get)),
+    );
 
     // TypeScript AMD modules are not named
   } else {
